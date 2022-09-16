fc39d66348ad7612efae6d97e1dfeac8bf639698
prashant <prashant@increasingly.com>
2022-09-16 16:56:10 +0530

    ganni sonarQube/Sonarcloud reports

--- gAN1uk8.js
+++ gAN1uk8.js
@@ -7493,7 +7493,6 @@
                                 }
                             }
                         }
-
                     }
                     if (clickedElement.classList.contains('remove_link') == trueflag && INC.config.pageType == "cartPage") {
                         setTimeout(function () {
@@ -7523,7 +7522,6 @@
             }
         }
     }
-
     let clientColor = document.querySelector('.product-attributes .product-attributes__item--color .js-custom-select');
     let clientSize = document.querySelector('.product-attributes .product-attributes__item--size .js-custom-select');
     if (clientColor != null){
